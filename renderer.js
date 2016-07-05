bc7959c8f6f93500a787462da951fdd1c8607cb1
David Figatner <david@yopeyopey.com>
2016-07-05 22:28:41 +0800

    That did not work. Reverting.

--- renderer.js
+++ renderer.js
@@ -51,7 +51,6 @@ function Renderer(options)
         for (var style in options.styles)
         {
             this.div.style[style] = options.styles[style];
-            this.canvas.style[style] = options.styles[style];
         }
     }
     this.width = 0;
